9519d0a655aa457c71e94b922e11e784b5efcbc3
unknown <enzo.graizeau11@gmail.com>
2025-01-06 11:38:04 +0100

    modif js 2

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const baliseTitreH4 = document.querySelector("h4")
-baliseTitreH4.style.background = "cyan"
+// const baliseTitreH4 = document.querySelector("h4");
+// baliseTitreH4.style.background = "cyan";
+
+
+const questionContainer = document.querySelector(".onClick");
+console.log(questionContainer);
+
+
+questionContainer.addEventListener("click", () => {
+    // console.log("J'ai cliqué");
+    // questionContainer.style.borderRadius = "25px";
+    // questionContainer.classList.add("questionOnClick");
+    // questionContainer.classList.toggle("questionOnClick");
+})
